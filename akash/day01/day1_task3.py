dce319d3a8f131cafec3c81ed01043180ce73154
akashpatel-bit <akash.patel@startappss.com>
2026-03-25 15:38:47 +0530

    Update day1_task3.py

--- akash/day01/day1_task3.py
+++ akash/day01/day1_task3.py
@@ -1,7 +1,22 @@
-arr = [2,7,11,15]
-target = 9
+#Q check time complexity in set vs list while searching for element
+# list example
+numbers_list = [10, 20, 30, 40, 50]
 
-for i in range(len(arr)):
-    for j in range(i+1, len(arr)):
-        if arr[i] + arr[j] == target:
-            print([i, j])
+# checking element in list
+if 30 in numbers_list:
+    print("Found in list")
+else:
+    print("Not found in list")
+
+
+# set example
+numbers_set = {10, 20, 30, 40, 50}
+
+# checking element in set
+if 30 in numbers_set:
+    print("Found in set")
+else:
+    print("Not found in set")
+    
+#time complexity in list is O(n) and in set is O(1) bcz set uses hash lookup    
+    
